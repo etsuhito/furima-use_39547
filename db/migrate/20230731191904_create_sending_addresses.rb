f2b152ae0c88279fdddec51735cc402a2b419e7c
etsuhito <137661300+etsuhito@users.noreply.github.com>
2023-08-01 19:15:41 +0900

    テーブル追加作成・アソシエーション

---
+++ db/migrate/20230731191904_create_sending_addresses.rb
@@ -0,0 +1,14 @@
+class CreateSendingAddresses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :sending_addresses do |t|
+      t.string :post_code, null: false
+      t.integer :prefecture_id, null: false
+      t.string :city,  null: false
+      t.string :street,  null: false
+      t.string :building_name
+      t.string :phone_num,  null: false
+      t.references :order, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
